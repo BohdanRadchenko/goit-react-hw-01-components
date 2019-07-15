158c5b61035c80da8e9e1de6c556f83b39415c9b
BohdanRadchenko <radchenkobs@gmail.com>
2019-07-15 14:28:26 +0300

    added homewor 01 components completed

--- src/components/PricingItem/PricingItem.js
+++ src/components/PricingItem/PricingItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import style from './PricingItem.module.css';
 
-const PricingItem = ({ label, icon, capacity, price, description }) => {
+const PricingItem = ({ label, icon, capacity, price, description, color }) => {
   return (
     <>
       <div className={style.pricingItem}>
@@ -15,12 +15,20 @@ const PricingItem = ({ label, icon, capacity, price, description }) => {
         <p className={style.capacity}>{capacity}</p>
         <p className={style.description}>{description}</p>
         <p className={style.price}>${price}/MO</p>
-        <button type="button" className={style.button}>
+        <button
+          type="button"
+          className={style.button}
+          style={{ backgroundColor: color }}
+        >
           Get Started
         </button>
       </div>
     </>
   );
+};
+
+PricingItem.defaultProps = {
+  color: '#c0c0c0',
 };
 
 PricingItem.propTypes = {
@@ -29,6 +37,7 @@ PricingItem.propTypes = {
   capacity: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
   description: PropTypes.string.isRequired,
+  color: PropTypes.string,
 };
 
 export default PricingItem;
